a27f81809693ed0754940b8a375ee842412a7afc
“meio” <meio134340@gmail.com>
2026-05-17 14:02:47 +0900

    CSSの修正：max-widthの指定（430px）

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
+import SplashScreen from "./_components/SplashScreen";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
-       
+      <body className="app-body">
+        <SplashScreen />
         <Header />
         {children}
       </body>
